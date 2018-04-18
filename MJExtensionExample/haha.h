9a4219a6a2543c7e240d6bd5845d6b6ace291d7f
shaopenglei <923432786@qq.com>
2018-04-18 17:36:52 +0800

    haha

---
+++ MJExtensionExample/haha.h
@@ -0,0 +1,13 @@
+//
+//  haha.h
+//  MJExtensionExample
+//
+//  Created by 邵朋磊 on 2018/4/18.
+//  Copyright © 2018年 小码哥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface haha : NSObject
+
+@end
---
+++ MJExtensionExample/haha.m
@@ -0,0 +1,13 @@
+//
+//  haha.m
+//  MJExtensionExample
+//
+//  Created by 邵朋磊 on 2018/4/18.
+//  Copyright © 2018年 小码哥. All rights reserved.
+//
+
+#import "haha.h"
+
+@implementation haha
+
+@end
